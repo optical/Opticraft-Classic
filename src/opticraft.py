0418052b53fe72ae0c5ecbab6ffea4ebd000364c
Jared Klopper <jaredklopper@gmail.com>
2010-10-13 03:05:35 +0000

    *3rd time lucky?!

--- src/opticraft.py
+++ src/opticraft.py
@@ -1,6 +1,6 @@
 '''Initial draft of OptiCraft server for Minecraft classic'''
 from servercontroller import ServerController
- 
+
 if __name__ == "__main__":
     ServerControl = ServerController()
     ServerControl.run()
