e853731b5140210e5525373ba7ec2ec785905217
opticalza <opticalza@a203bda2-3820-4cc8-b9d4-dbaaa8a06af9>
2010-10-13 02:44:48 +0000

    * Test

--- src/opticraft.py
+++ src/opticraft.py
@@ -1,9 +1,3 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-__author__="optical"
-__date__ ="$30/09/2010 8:17:39 PM$"
-
 '''Initial draft of OptiCraft server for Minecraft classic'''
 from servercontroller import ServerController
 
