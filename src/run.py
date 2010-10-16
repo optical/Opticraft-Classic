3a4fc18b9ba03d3fb5d482c8c3901b4318d9ab08
Jared Klopper <jaredklopper@gmail.com>
2010-10-16 11:09:14 +0000

    * Implemented quick and easy crash logging.  - All Crash stacktraces will now be written to "Crashlog.txt" should one occur!

--- src/run.py
+++ src/run.py
@@ -1,5 +1,15 @@
 '''Initial draft of OptiCraft server for Minecraft classic'''
 from servercontroller import ServerController
+import traceback
+import time
 if __name__ == "__main__":
-    ServerControl = ServerController()
-    ServerControl.run()
+    try:
+        ServerControl = ServerController()
+        ServerControl.run()
+    except:
+        fHandle = open("CrashLog.txt","a")
+        fHandle.write("="*15 + "\n")
+        fHandle.write("Crash date: %s" %time.strftime("%c", time.gmtime()))
+        fHandle.write("="*15 + "\n")
+        traceback.print_exc(file=fHandle)
+        fHandle.close()
