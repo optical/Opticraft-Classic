72a8f2dafed17cc91f414d44ab0d0ac863cb5b87
Jared Klopper <jaredklopper@gmail.com>
2010-10-22 02:04:47 +0000

    * opcodes and blocktype merged into constants.py

--- src/optisockets.py
+++ src/optisockets.py
@@ -1,6 +1,3 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-import time
 import socket
 from select import select
 from player import Player
--- src/player.py
+++ src/player.py
@@ -1,6 +1,6 @@
 import hashlib
 from opticraftpacket import OptiCraftPacket
-from opcodes import *
+from constants import *
 class Player(object):
     #Constructor is located at the bottom
     def ProcessPackets(self):
