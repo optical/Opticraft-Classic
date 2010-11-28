475f7fdeb95b15a4d613aa46aca87f7214eafc04
opticalza <opticalza@a203bda2-3820-4cc8-b9d4-dbaaa8a06af9>
2010-11-28 08:39:30 +0000

    * Changing the default world will now allow the old default world to go idle as it should * Changed the grey in the irc relay to a more readable one * Renaming a world will now rename the backups folder for that world as well * Owners can now remove worlds with /removeworld * Fixed bug which caused the players command to occasionally miss a few players * Fixed a possible memory leak/circular reference (Unsure if the Garbage collector was picking it up. I suspect it was though)

--- src/ircrelay.py
+++ src/ircrelay.py
@@ -53,7 +53,7 @@ class RelayBot(IRCClient):
         self.ColourMap["&4"] = '%s4' %RelayBot.COLOUR_CODE #Dark red
         self.ColourMap["&5"] = '%s6' %RelayBot.COLOUR_CODE #Purple
         self.ColourMap["&6"] = '%s8' %RelayBot.COLOUR_CODE #gold
-        self.ColourMap["&7"] = '%s15' %RelayBot.COLOUR_CODE #Grey
+        self.ColourMap["&7"] = '%s14' %RelayBot.COLOUR_CODE #Grey
         self.ColourMap["&8"] = '%s14' %RelayBot.COLOUR_CODE #Dark Grey
         self.ColourMap["&9"] = '%s12' %RelayBot.COLOUR_CODE #blue
         self.ColourMap["&a"] = '%s3' %RelayBot.COLOUR_CODE #Green
